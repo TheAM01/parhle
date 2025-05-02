99ead8e401c15b76a6d0268c1010fa2513dcdd3c
TheAM01 <onevisionofficial55@gmail.com>
2025-05-02 17:23:22 +0500

    1.2.2

--- app/user/profile/page.js
+++ app/user/profile/page.js
@@ -1,32 +1,3 @@
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
-import Spinner from "@/components/ui/spinner.js"
-import { SignOutButton } from "@/components/ui/button";
-
 export default function Profile() {
-    const router = useRouter();
-
-    const status = "unauthenticated";
-
-    useEffect(() => {
-        if (status === "unauthenticated") {
-            router.replace("/user/signin?login-first=true&redirect-to=%2Fuser%2Fprofile");
-        }
-    }, [status, router]);
-
-    if (status === "loading" || status === "unauthenticated") {
-        return (
-            <div className="text-white h-screen items-center justify-center w-full">
-                <Spinner/>
-            </div>
-        )
-    }
-
-    return (
-        <div className="text-white h-screen items-center justify-center w-full flex-col">
-            Signed in as a nigga<br/>
-            <SignOutButton/>
-        </div>
-    );
-
+    return <div>Dead</div>
 }
